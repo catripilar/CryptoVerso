4a996b4d2d12098c4fa125c66234f8ecd8396685
Catripilar <70659123+catripilar@users.noreply.github.com>
2023-10-13 22:03:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var balances = 0;
 var conectado = false;
 var free = false;
+var tokenId = 0;
+var ID = 0;
 const abi = [{"inputs":[{"internalType":"address","name":"orl","type":"address"},{"internalType":"uint8","name":"percent","type":"uint8"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"ERC721EnumerableForbiddenBatchMint","type":"error"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"address","name":"owner","type":"address"}],"name":"ERC721IncorrectOwner","type":"error"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ERC721InsufficientApproval","type":"error"},{"inputs":[{"internalType":"address","name":"approver","type":"address"}],"name":"ERC721InvalidApprover","type":"error"},{"inputs":[{"internalType":"address","name":"operator","type":"address"}],"name":"ERC721InvalidOperator","type":"error"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"ERC721InvalidOwner","type":"error"},{"inputs":[{"internalType":"address","name":"receiver","type":"address"}],"name":"ERC721InvalidReceiver","type":"error"},{"inputs":[{"internalType":"address","name":"sender","type":"address"}],"name":"ERC721InvalidSender","type":"error"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ERC721NonexistentToken","type":"error"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"ERC721OutOfBoundsIndex","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"add_addr","type":"address"}],"name":"AddParceiros","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"i","type":"uint256"}],"name":"DelParceiros","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"NFTData","outputs":[{"components":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"uint256","name":"expired","type":"uint256"},{"internalType":"uint256","name":"minted","type":"uint256"},{"internalType":"uint256","name":"cargo","type":"uint256"},{"internalType":"uint256","name":"cashBack","type":"uint256"},{"internalType":"uint256","name":"DiscordId","type":"uint256"}],"internalType":"struct NFTInfo[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"ParceirosData","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"creator","type":"string"},{"internalType":"uint256","name":"i","type":"uint256"}],"name":"Price","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"uint256","name":"discord_id","type":"uint256"}],"name":"active","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"creator","type":"string"},{"internalType":"address","name":"creator_addr","type":"address"},{"internalType":"uint256","name":"cargo","type":"uint256"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"cashBack","type":"uint256"},{"internalType":"uint256","name":"time","type":"uint256"},{"internalType":"uint8","name":"decimal","type":"uint8"}],"name":"ads","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"baseURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"burning","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"string","name":"creator","type":"string"},{"internalType":"uint256","name":"discord_id","type":"uint256"},{"internalType":"uint256","name":"i","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"creator","type":"string"}],"name":"plans","outputs":[{"components":[{"internalType":"address","name":"owner_p","type":"address"},{"internalType":"uint256","name":"cargo","type":"uint256"},{"internalType":"uint256","name":"timestamp","type":"uint256"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256","name":"cashBack","type":"uint256"},{"internalType":"uint8","name":"decimal","type":"uint8"}],"internalType":"struct Planos[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"creator","type":"string"},{"internalType":"uint256","name":"i","type":"uint256"}],"name":"rmv","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_baseURI","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"}];
 const contrato_addr = "0xc78851a8C20a7829617D4FeDaFe1A7284FdF847A";
 async function conectar() {
@@ -35,6 +37,18 @@ async function conectar() {
             .catch(error => {
                 console.error('Erro ao buscar o JSON:', error);
             });
+            const nfts = await contract.methods.NFTData().call();
+            for (var i = 0; i < nfts.length; i++) {
+                if(nfts[i].owner == contas[0]){
+                    tokenId = nfts[i].tokenId;
+                    if (nfts[i].DiscordId != 0){
+                        ID = nfts[i].DiscordId;
+                        document.getElementById("discordId").style.display = "none";
+                        document.getElementById("discordIdButton").style.display = "none";
+                    }
+                    document.getElementById("expire").textContent = `data de expiração: ${formatUnixTime(nfts[i].expired)}`;
+                }
+            }
             if (infoParam) {
                 const planos = await contract.methods.plans(infoParam).call();
                 document.getElementById("walletInput").value = contas[0];
@@ -56,7 +70,31 @@ async function conectar() {
             } else {
                 console.log("nao encontrado")
             }
+            if (tokenId != 0){
+                document.getElementById("sec_owner").style.display = "block";
+            }
         }
+    }
+}
+async function burn() {
+    if (conectado == true && tokenId != 0 && ID != 0){
+        const web3 = new Web3(window.ethereum);
+        const contas = await web3.eth.getAccounts();
+        const contract = await new web3.eth.Contract(abi,contrato_addr);
+        await contract.methods.burning(tokenId).send({from: contas[0]})
+        .then(_ => {ID = id_discord;alert("NFT queimada com sucesso!")})
+        .catch(_ => {alert("erro ao queimar sua NFT..")})
+    }
+}
+async function ativar() {
+    const id_discord = document.getElementById("discordId").value;
+    if (conectado == true && tokenId != 0 && ID == 0){
+        const web3 = new Web3(window.ethereum);
+        const contas = await web3.eth.getAccounts();
+        const contract = await new web3.eth.Contract(abi,contrato_addr);
+        await contract.methods.active(tokenId,id_discord).send({from: contas[0]})
+        .then(_ => {ID = id_discord;alert("NFT ativada com sucesso!")})
+        .catch(_ => {alert("erro ao ativar sua NFT..")})
     }
 }
 async function payable(element) {
@@ -75,12 +113,12 @@ async function payable(element) {
             const contract = await new web3.eth.Contract(abi,contrato_addr);
             if (free == true && time < 7){
                 await contract.methods.mint(carteira,criador,id_discord,plano).send({from: contas[0]})
-                .then(res => console.log('Success', res))
-                .catch(err => console.log(err))  
+                .then(_ => {ID = id_discord;alert("NFT mintada com sucesso!")})
+                .catch(_ => {alert("erro ao mintar sua NFT..")})
             }else{
                 await contract.methods.mint(carteira,criador,id_discord,plano).send({from: contas[0],value: price})
-                .then(res => console.log('Success', res))
-                .catch(err => console.log(err))  
+                .then(_ => {ID = id_discord;alert("NFT mintada com sucesso!")})
+                .catch(_ => {alert("erro ao mintar sua NFT..")})
             }
         }
     }
@@ -101,4 +139,13 @@ function transformarNumero(numero,casasDecimais) {
 function getURLParameter(name) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(name);
+}
+function formatUnixTime(unixTime) {
+    const date = new Date(unixTime * 1000);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+
+    return `${year}/${month}/${day}:${hours}`;
 }
