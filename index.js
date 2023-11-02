cecd6e31d99dc7d8d3df26b2ddd47ffb952752b3
Catripilar <70659123+catripilar@users.noreply.github.com>
2023-11-02 01:42:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -227,14 +227,16 @@ async function payable(element) {
             alert("Fundos insuficientes")
         }
         if (permit){
-            if (free == true && time < 7){
+            if (free == true && time < 7 && carteira != contas[0]){
                 carteira = document.getElementById("Wallet").value;
-                if (carteira != contas[0]){
-                    id_discord = 0;
+                const havenft = await contract.methods.balanceOf(carteira);
+                if(havenft == 0){
+                    await contract.methods.mint(carteira,criador,tokenId,0,plano).send({from: contas[0]})
+                    .then(_ => {ID = id_discord;alert("NFT do criador mintada com sucesso!")})
+                    .catch(_ => {alert("erro ao mintar NFT como criador..")})
+                }else{
+                    alert("usuario já possui nft..")
                 }
-                await contract.methods.mint(carteira,criador,tokenId,id_discord,plano).send({from: contas[0]})
-                .then(_ => {ID = id_discord;alert("NFT do criador mintada com sucesso!")})
-                .catch(_ => {alert("erro ao mintar NFT como criador..")})
             }else{
                 await contract.methods.mint(carteira,criador,tokenId,id_discord,plano).send({from: contas[0],value: price})
                 .then(_ => {
